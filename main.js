060b8f707ece858491128e78de5fa73f88b4f459
rizkyy20 <muhammadrizkyyamin@gmail.com>
2024-08-25 21:55:14 +0800

    nambahin footer doang sisanya nnti di cicil wkwk

---
+++ main.js
@@ -0,0 +1,11 @@
+function ig() {
+    window.location.href = "https://instagram.com/anotherfrz"
+}
+
+function github() {
+    window.location.href = "https://www.github.com/rizkyy20"
+}
+
+function tiktok() {
+    window.location.href = "https://www.tiktok.com/@froz208"
+}
